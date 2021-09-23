51e6ca264785de367160a1fdcb44b67588f96950
Matias Aguilera <malbertoaguilera@outlook.es>
2021-09-22 21:26:35 -0300

    Modificacion firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,13 +4,22 @@ import  'firebase/firestore';
 
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyDmhRDEcmHFsDXK84xrKAgxf86rqJuICWE",
+  authDomain: "pug-drinks-react.firebaseapp.com",
+  projectId: "pug-drinks-react",
+  storageBucket: "pug-drinks-react.appspot.com",
+  messagingSenderId: "997658094238",
+  appId: "1:997658094238:web:62c3ac004cff95d27da98f"
 };
 
 // Initialize Firebase
